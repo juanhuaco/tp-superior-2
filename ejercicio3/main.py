da994e4569816aff0b6d2666860ba1eaeff89fe2
juanhuaco <juanpablohuacodelcarpio@gmail.com>
2022-09-26 15:40:23 -0300

    b is ready

--- ejercicio3/main.py
+++ ejercicio3/main.py
@@ -1,7 +1,37 @@
-#here comes the entiere code?
+#here comes the entiere code
 import numpy as np
 from VanDerMonde import getVanDerMonde
+from solveGauss import solve_gauss, getResidue
+import matplotlib.pyplot as plt
+
+def vanDerMondeResidue(n):
+    A, y = getVanDerMonde(n)
+    sol = solve_gauss(A, y)
+    r = getResidue(A, sol, y)
+    r = np.linalg.norm(r, 2)
+    r = np.log10(r)
+    return r
+
+def main():
+    n=80
+    img=np.zeros(n)
+    dom=np.arange(1,n+1)
+    for i in range(1, n+1):
+        img[i-1] = vanDerMondeResidue(i)
+    
+    
+    plt.title("VDM residue by n")
+    plt.xlabel("n")
+    plt.ylabel("residue in norm 2")
+    plt.plot(dom, img)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+    
 
 
-A, y = getVanDerMonde(3)
-print(f'A = {A} - y = {y}')
+
+
+
+
--- ejercicio3/solveGauss.py
+++ ejercicio3/solveGauss.py
@@ -1,21 +1,18 @@
 import numpy as np
 from sympy import *
+
 def solve_upper_triang(A,b):
-    #[[1, 2],[0, 2]] ; [1, 1 ] x=0, y=1/2
+    dim=A.shape[0]
+    sol=np.zeros_like(b)
     
-    dim=A.shape[0] #->2
-
-    sol=np.zeros_like(b) #->[0, 0]
-
-    for i in range(dim): # i->0
-        suma=b[dim-i-1] #->last element - sum = 1
-        for j in range(i+1): #j=0
-            suma=suma - A[dim-i-1,dim-j-1]*sol[dim-j-1]#->A[1,1]*0
-            sol[dim-i-1]=suma/A[dim-i-1,dim-i-1]#sol[1]=0/A[1,1]
-            #print(f'i: {i} - j: {j} - suma: {suma} - sol{sol}')
+    for i in range(dim):
+        suma=b[dim-i-1]
+        for j in range(i+1):
+            
+            suma-=A[dim-i-1,dim-j-1]*sol[dim-j-1]
     
+        sol[dim-i-1]=suma/A[dim-i-1,dim-i-1]
     return sol
-
 
 
 def solve_gauss(A,y): 
@@ -31,3 +28,6 @@ def solve_gauss(A,y):
 
     x=solve_upper_triang(A,y)
     return x
+
+
+def getResidue(A, x, b): return np.dot(A, x) - b
